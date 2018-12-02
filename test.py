36efb61fc01c99bbb46e28ae150ea18835ffa202
noel_merino@yahoo.com.mx <noel_merino@yahoo.com.mx>
2018-12-01 19:35:02 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
+# Este script es sólo un esqueleto y necesita muchas muchas modificaciones para funcionar.
+# Lo tengo sólo como ejemplo.
+
 import usb.core
 import usb.util
 
-# find the device
+# Encuentra el Vorago MO-404
 dev = usb.core.find(idVendor=0x1d57, idProduct=0xfa0a)
 
-# detach the kernel driver
+# Desconecta del kernel
 dev.detach_kernel_driver(1)
 usb.util.claim_interface(dev,1)
 dev.set_interface_altsetting(interface=1,alternate_setting=0)
 
-# set a hardcoded colour
-colors = [0x00, 0x00, 0xFF]
+# Blink on/off
+blink = [0x00, 0x00, 0xFF]
 
 # concatenate the colours into the expected 8 bytes
 data = [0x07, 0x0a, 0x01, 0x00] + colors + [0x00]
